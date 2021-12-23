4849fd7b05409912d25725ff7ef7b4e0b106c8d4
Jeff <jalmache@addi.com>
2021-12-22 20:42:16 -0500

    removed hbase jar from sh script

--- project/script/download_db_packages.sh
+++ project/script/download_db_packages.sh
@@ -2,5 +2,4 @@
 
 cd package
 wget https://jdbc.postgresql.org/download/postgresql-42.2.22.jar .
-wget https://repo.hortonworks.com/content/repositories/releases/com/hortonworks/shc-core/1.1.1-2.1-s_2.11/shc-core-1.1.1-2.1-s_2.11.jar .
 cd -
