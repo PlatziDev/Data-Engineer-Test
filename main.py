4736d0ed7213150b222095962bd3768800fce337
andreescastano <andrees.castano@gmail.com>
2022-04-16 10:13:29 -0500

    Se crea la primera version de la ETL,con tres funciones Extract, Transform y Load

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import boto3
 import pandas as pd
 import redshift_connector
-
+import sqlalchemy
 
 from datetime import datetime
 
+
 # Declare valiables
 date = str(datetime.now())
-bucket = 'mibucketredshift-ac'
-courses_file = 'course_completes.csv'
-AWS_SERVER_PUBLIC_KEY = 'AKIAXMQDSWTQVRRW5I46'
-AWS_SERVER_SECRET_KEY = '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'
 
 
 def connect_to_db():
     try:
         print(date + ' - Connecting to Redshift...')
         con = redshift_connector.connect(  host='redshift-cluster.cjdkillol2ft.us-east-1.redshift.amazonaws.com',
-                                            port=5439,
-                                            database='dev',
-                                            user='user_platzi',
-                                            password='Platzi123' )
+                                           port=5439,
+                                           database='platzi',
+                                           user='user_platzi',
+                                           password='Platzi123' )
         print(date + ' - Connected to Redshift...')
-        print(str(con))
         with con.cursor() as cursor:
-            cursor.execute("SELECT * FROM public.category")
+            cursor.execute("SELECT * FROM dwh.category")
             result: tuple = cursor.fetchall()
             print(result)
     except Exception as e:
@@ -32,11 +28,15 @@ def connect_to_db():
 
 
 
-def extract_from_s3():
+def extract():
+    bucket = 'mibucketredshift-ac'
+    courses_file = 'courses_events.csv'
+    subscriptions_file = 'subscriptions_events.csv'
+    AWS_SERVER_dwh_KEY = 'AKIAXMQDSWTQVRRW5I46'
+    AWS_SERVER_SECRET_KEY = '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'
     try:
         print(date + ' - Connecting to S3...')
-        s3 = boto3.client('s3', aws_access_key_id= AWS_SERVER_PUBLIC_KEY, aws_secret_access_key= AWS_SERVER_SECRET_KEY)
-        # s3_resource = boto3.resource('s3', aws_access_key_id= AWS_SERVER_PUBLIC_KEY, aws_secret_access_key= AWS_SERVER_SECRET_KEY)
+        s3 = boto3.client('s3', aws_access_key_id= AWS_SERVER_dwh_KEY, aws_secret_access_key= AWS_SERVER_SECRET_KEY)
         print(date + ' - Connection established!')
     except Exception as e:
         print(date + ' - Connection error: ' + str(e))
@@ -44,32 +44,70 @@ def extract_from_s3():
     try:
         print(date + ' - Validating files existence')
         obj_courses = s3.get_object(Bucket = bucket, Key = courses_file)
-        # obj_site = s3.get_object(Bucket = bucket, Key = result_file_site)
+        # obj_subscriptions = s3.get_object(Bucket = bucket, Key = subscriptions_file)
         print(date + ' - All files were found!')
     except Exception as e:
         print(date + ' - Validation has an error: ' + str(e))
 
     try:
         print(date + ' - Generating dataframes')
-        df_courses = pd.read_csv(obj_courses['Body'], delimiter=';')
-        # df_site = pd.read_csv(obj_site['Body'], delimiter='|')
+        df_courses = pd.read_csv(obj_courses['Body'], delimiter=';',header = 0, names=['id_event', 'first_name', 'last_name', 'email', 'phone', 'class', 'type_event', 'date_event','time'])
+        # df_subscriptions = pd.read_csv(obj_subscriptions['Body'], delimiter=';',header = 0, names=['id_event', 'first_name', 'last_name', 'email', 'phone', 'class', 'type_event', 'date_event','time'])
         print(date + ' - Dataframes generated successfully!')
-        print(df_courses)
     except Exception as e:
         print(date + ' - Dataframe generation has an error: ' + str(e))
 
-
-def load_to_stg():
-    pass
+    return df_courses #, df_subscriptions
 
 
+def transform(df_courses):
+    try:
+        print(date + ' - Cleaning data')
 
-def run():
-    pass
+        # Students Dimention
+        df_dimStudents = df_courses.loc[:,['first_name','last_name','email','phone']].drop_duplicates(subset=['first_name','last_name','email','phone'])
+        df_dimStudents = df_dimStudents.replace( '-', '', regex=True)
 
+        # Class Dimention
+        df_dimClass = df_courses.loc[:,['class']].drop_duplicates(subset=['class'])
+        df_dimClass = df_dimClass.replace( '-', '', regex=True)
+
+        print(date + ' - Data has been cleaned!')
+    except Exception as e:
+        print(date + ' - Error cleaning data: ' + str(e))
+
+    return df_dimStudents,df_dimClass
+
+# def test():
+def load(df_clean):
+    try:
+        print(date + ' - Connecting to Redshift...')
+        con = redshift_connector.connect(  host='redshift-cluster.cjdkillol2ft.us-east-1.redshift.amazonaws.com',
+                                           port=5439,
+                                           database='platzi',
+                                           user='user_platzi',
+                                           password='Platzi123' )
+        print(date + ' - Connected to Redshift!')
+        with con.cursor() as cursor:
+            print(date + ' - Inserting dim_Students...')
+            cursor.execute("DELETE FROM dwh.dim_Students")
+            cursor.executemany("INSERT INTO dwh.dim_Students (first_name, last_name, email, phone) VALUES (%s, %s, %s, %s)", df_clean.values.tolist())
+            con.commit()
+            print(date + ' - Inserted dim_Students!')
+    except Exception as e:
+        print(date + ' - Connection error: ' + str(e))
+
+
+
+def main():
+    # test()
+    df_extract = extract()
+    df_clean = transform(df_extract)
+    print(df_clean)
+    # load(df_clean)
 
 
 if __name__ == '__main__':
-    connect_to_db()
+    main()
 
 
